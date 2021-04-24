352a2685e2dc0ad5ad1dfee19001f8f05731876c
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2021-04-24 17:17:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HI = open("hello.txt", "r", encoding='utf-8').read()
 RULE = open("rule.txt", "r", encoding='utf-8').read()
 RULE_BOMB = open("rule_bomb.txt", "r", encoding='utf-8').read()
 SHOP = open("shop.txt", "r", encoding='utf-8').read()
-TOKEN = "ODM0ODU3OTUwMjE2MzIzMDcy.YIHADg.0Tee001TtVKg9dOss6lfTr4hyhg"
+TOKEN = "ODM0ODU3OTUwMjE2MzIzMDcy.YIHADg.n-bdqxT5sGWb-5ApTJyaxSeiRco"
 MENU_CASINO = False
 START = True
 MENU_MINES = False
